80c4c36bca0cee8465a27fcb2333ed5bdfaa4cdf
ATHIRA <127323277+unitiaathiras@users.noreply.github.com>
2023-04-17 19:49:58 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+
+''' 1. Write a Python program to print the following string in a specific format.
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,'''
+
+
+print('''Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star''')

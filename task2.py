0b581f7f71135d16dbbfe1d70d4efbd7432a6e09
ATHIRA <127323277+unitiaathiras@users.noreply.github.com>
2023-04-19 10:39:20 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 '''Write a program which will find all such numbers which are divisible by 7 but are not a m
 ultiple of 5 between 2000 and 3200(both included). The numbers obtained should be printed
-in a comma seperated sequence on a songle line.'''
+in a comma seperated sequence on a single line.'''
 
 ans = []
 num = range(2000, 3201)
